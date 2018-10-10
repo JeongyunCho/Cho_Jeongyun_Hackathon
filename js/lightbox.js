5d6b84814c897e2897418dd7bba3c9444634bdf7
Lee Changseok <leechangseok@Lees-MBP.fc.ca>
2018-10-10 14:37:21 -0400

    lightbox Creadted

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,89 +1,37 @@
- //variables
- let clickImg = document.querySelectorAll(".clickme"),
-     imageBox = document.querySelector("#imgBox"),
-     close = document.querySelector("#close"),
-     lightBox = document.querySelector("#lightbox"),
-     nav = document.querySelector("#mainNav"),
-     name = document.querySelector("#name"),
-     desc = document.querySelector("#desc"),
-     leftBtn = document.querySelector("#left"),
-     rightBtn = document.querySelector("#right"),
-     idx=0;
+//Get the modal
+var modal = document.querySelector('#myModal');
 
-//functions
-//imageSwap changes image in lightbox
- imageSwap=(e)=>{
-   lightBox.className = "open";
-  nav.style.visibility='hidden';
-  imageBox.src = e.target.src;
-  nameSwap(portfolioData);
-  textSwap(portfolioData);
- };
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var modalImg = document.querySelector("#img01");
+var images = document.querySelectorAll('.portFolioImg');
+var viewBut = document.querySelectorAll('.myImg');
 
-
-  nameSwap=(data)=>{
-    if (imageBox.src.match("cho")){
-      idx=0;
-    }else{
-      idx=1;
+function swapImage(index) {
+    modal.style.display = "block";
+        for(var i=0; i<images.length; i++){
+            if(index===i){
+            modalImg.src=images[i].src;
+        }
     }
-    name.innerText=data.arrName[idx];
-  }
-  textSwap=(data)=>{
-    if (imageBox.src.match("cho1")){
-      idx=0;
-    }
-    if (imageBox.src.match("cho2")){
-      idx=1;
-    }
-    if (imageBox.src.match("andrino1")){
-      idx=2;
-    }
-    if (imageBox.src.match("andrino2")){
-      idx=3;
-    }
-    if (imageBox.src.match("andrino3")){
-      idx=4;
-    }
-    
-    desc.innerText=data.arrText[idx];  
-}
-//button function change Pic
-  next=(data)=>{
-  idx++;
-  if(idx==5)idx=0;
-  imageBox.src = "images/"+data.imgArr[idx]+".png";
-  nameSwap(portfolioData);
-  textSwap(portfolioData);
-}
-
-prev=(data)=>{
-  idx--;
-  if(idx==-1)idx=4;
-  imageBox.src = "images/"+data.imgArr[idx]+".png";
-  nameSwap(portfolioData);
-  textSwap(portfolioData);
-}
-
-
- closeBtn=()=>{
-   lightBox.className = "";
-   nav.style.visibility='visible';
- };
-
- closeClick=(e)=>{
-  // console.log(e.target);
-if (e.target.id == "lightbox_content") {
-lightBox.className = "";
-nav.style.visibility='visible';
-}
 };
 
-//eventListeners
-for(let i=0; i<clickImg.length; i++){
- clickImg[i].addEventListener("click",imageSwap,false);
-}
-close.addEventListener("click",closeBtn,false);
-lightBox.addEventListener("click",closeClick,false);
-leftBtn.addEventListener("click",() => { prev(portfolioData);},false);
-rightBtn.addEventListener("click",() => { next(portfolioData);},false);
+
+        
+
+
+for(i=0; i<images.length; i++){
+    viewBut[i].addEventListener('click',swapImage.bind(null, i),false);
+};
+
+
+
+// Get the <span> element that closes the modal
+var span = document.querySelectorAll(".close")[0];
+
+// When the user clicks on <span> (x), close the modal
+
+function closeBox(){
+    modal.style.display = "none";
+};
+
+span.addEventListener('click',closeBox,false);
